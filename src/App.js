9b4bc74ac10c3c7d1fea0f7d571ea2f266a881ef
kaede0902 <kaede0920js@gmail.com>
2020-10-14 03:24:55 +0900

    add mui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import NameForm from './lib/NameForm'
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div>
+      <Button variant='contained' color='secondary'>PRESS ME</Button>
+      <Button >PRESS ME</Button>
       <NameForm/>
     </div>
   );
--- src/lib/NameForm.jsx
+++ src/lib/NameForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import '../styles/global.css'
+import { FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+
 export default function NameForm() {
 
   const [theirName, setTheirName] = useState('theirName');
@@ -8,16 +10,26 @@ export default function NameForm() {
 
   return (
     <div className='main'>
-      <input 
-        type="text"
-        value={theirName}
-        onChange={ e => setTheirName( e.target.value ) }
-      />
-      <input 
-        type="text"
-        value={myName}
-        onChange={ e => setMyName( e.target.value ) }
-      />
+      <FormControl>
+        <InputLabel htmlFor="my-input">相手の名前</InputLabel>
+        <Input 
+          id="my-input" 
+          aria-describedby="my-helper-text" 
+          value={theirName}
+          onChange={ e => setTheirName( e.target.value ) }
+        />
+        <FormHelperText id="my-helper-text">相手の名前を入れてね</FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">自分の名前</InputLabel>
+        <Input 
+          id="my-input" 
+          aria-describedby="my-helper-text" 
+          value={myName}
+          onChange={ e => setMyName( e.target.value ) }
+        />
+        <FormHelperText id="my-helper-text">自分の名前を入れてね</FormHelperText>
+      </FormControl>
 
       <div>
         output: <br/>
